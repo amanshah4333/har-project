f7da548b1f3ee14e86c1c9b5f70704b9e151d0e1
Aman Ali <aman.shah4333@gmail.com>
2024-12-12 22:33:34 -0500

    removing test

--- tests/test_model.py
+++ tests/test_model.py
@@ -13,6 +13,3 @@ def test_dataset_preprocessing():
     assert len(y_train) == X_train.shape[0], "Mismatch in X_train and y_train length!"
     assert len(y_test) == X_test.shape[0], "Mismatch in X_test and y_test length!"
 
-def test_model_saving():
-    model = joblib.load("har_model.pkl")
-    assert model is not None, "Model failed to load!"
